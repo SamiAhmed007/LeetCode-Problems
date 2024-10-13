8c01e4f0eb211efac382d4a292cf318d9eb39b1b
Mohammed Sami Ahmed <84227987+SamiAhmed007@users.noreply.github.com>
2024-10-13 16:51:17 -0400

    Time: 39 ms (48.81%), Space: 16.3 MB (91.73%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,11 +1,15 @@
 class Solution:
+
+    def __init__(self):
+            self.dic={0:0,1:1}
+
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n==1:
-            return 1 
-        
-        return self.fib(n-1) + self.fib(n-2)
+        if n not in self.dic:
+            self.dic[n]=self.fib(n-1)+self.fib(n-2);
+
+        return self.dic[n]
+            
+    
         
 
         
