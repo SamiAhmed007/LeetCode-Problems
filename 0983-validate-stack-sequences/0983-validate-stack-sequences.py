b1ae21234e576edbe59722e3d510c6f53211cabf
Mohammed Sami Ahmed <84227987+SamiAhmed007@users.noreply.github.com>
2024-10-16 02:08:09 -0400

    Time: 70 ms (25.57%), Space: 16.8 MB (12.33%) - LeetHub

--- 0983-validate-stack-sequences/0983-validate-stack-sequences.py
+++ 0983-validate-stack-sequences/0983-validate-stack-sequences.py
@@ -4,12 +4,10 @@ class Solution:
         while len(pushed)>0:
             elem = pushed.pop(0)
             temp_stack.append(elem)
-            
             while len(temp_stack)>0 and temp_stack[-1] == popped[0]:
                 temp_stack.pop()
                 popped.pop(0)
-        print(pushed, popped,temp_stack)
-        # print(pushed, popped[::-1],temp_stack)
+        # print(pushed, popped,temp_stack)
         if temp_stack == popped[::-1]:
             return True
         else:
